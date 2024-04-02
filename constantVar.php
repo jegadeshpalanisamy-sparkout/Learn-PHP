9e411a03bd138c668de28ac3c8984e9dfc3c8d93
jegadeshpalanisamy-sparkout <jegadesh54321@gmail.com>
2024-04-02 18:33:12 +0530

    modified file

--- constantVar.php
+++ constantVar.php
@@ -1,11 +1,19 @@
 <?php
-    // define("name","jega");
-    // echo name;
-    // const name="janani";
-    // echo constant("name");
     
+   // echo name;
+    //echo constant("name1");
 
-    //constant array
+    // constant array
     // define("car",["red","black","blue"]);
     // echo(car[1]);
+
+    function dif()
+    {
+        echo name;
+        echo name1;
+
+    }
+    dif();
+    define("name","ram");
+    const name1="janani";
 ?>
