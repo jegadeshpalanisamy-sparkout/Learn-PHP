5583b722a97ee1a7ed391699b8ef950dcb82b116
jegadeshpalanisamy-sparkout <jegadesh.palanisamy@sparkouttech.com>
2024-04-21 19:42:13 +0530

    21-04-2024

--- Tasks/Checking the Key Value Pairs/script.js
+++ Tasks/Checking the Key Value Pairs/script.js
@@ -1,31 +1,15 @@
 
 
-    function updateValue() {
-
-    console.log("hii");
-
+function updateValue() 
+{
+    //console.log("hii");
     var selectedKey = document.getElementById("keySelection").value;
     console.log(selectedKey);
-    // var selectedValue = "<?php echo isset($_COOKIE[selectedKey]) ? $_COOKIE[selectedKey] : ''; ?>";
-    // console.log(selectedValue);
-    // document.getElementById("value").innerHTML = selectedValue;
-   const output= document.getElementById('value');
-   output.innerHTML=selectedKey;
+    
+    const output= document.getElementById('value');
+    output.innerHTML=selectedKey;
 
 } 
 
 
-// function updateValue() {
-//     console.log(hii);
-//     var selectedKey = document.getElementById("keySelection").value;
-//     var selectedValue = "<?php echo isset($_COOKIE[selectedKey]) ? $_COOKIE[selectedKey] : ''; ?>";
-//     document.getElementById("value").innerHTML = selectedValue;
-// }
-
-// function updateValue() {
-//     console.log(hii);
-//     var selectedKey = document.getElementById("keySelection").value;
-//     var selectedValue = "<?php echo isset($_COOKIE[selectedKey]) ? $_COOKIE[selectedKey] : ''; ?>";
-//     document.getElementById("value").innerHTML = selectedValue;
-// }
 
